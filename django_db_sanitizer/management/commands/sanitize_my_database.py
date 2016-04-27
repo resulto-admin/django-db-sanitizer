987a1129db31a38c49e7c0cbb7b7f9f7a8b0f438
Martin Gingras <mgingras@resulto.ca>
2016-04-27 13:11:46 -0400

    Preparing for 0.1.0 release

--- django_db_sanitizer/management/commands/sanitize_my_database.py
+++ django_db_sanitizer/management/commands/sanitize_my_database.py
@@ -1,27 +1,14 @@
-# from optparse import make_option
 import time
 
-from django.core.management.base import BaseCommand  # , CommandError
+from django.core.management.base import BaseCommand
 
 from django_db_sanitizer import sanitizer_registry
 
 
 class Command(BaseCommand):
-    # option_list = BaseCommand.option_list + (
-    #     make_option(
-    #         "--db-name",
-    #         action="store",
-    #         dest="db_name",
-    #         default="",
-    #         help="Name of the database to sanitize."),
-    # )
 
     def handle(self, *args, **options):
         start = time.time()
-        # TODO see if specifying the db with the command will work. Maybe not..
-        # db_name = options["db_name"]
-        # if not db_name:
-        #     raise CommandError("--db-name option value must be provided.")
 
         registry = sanitizer_registry.get_registry()
         for model_class, worker_list in registry.items():
